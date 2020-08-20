d1a958a0765d0b233e67eaf00243f6ef2ae56cdc
jarreleye <pawiwi@2980.com>
2020-08-20 16:08:58 +0800

    TODO: Fix later

--- cmd/think.go
+++ cmd/think.go
@@ -23,6 +23,7 @@ package cmd
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
@@ -30,15 +31,12 @@ import (
 // thinkCmd represents the think command
 var thinkCmd = &cobra.Command{
 	Use:   "think",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "A brief description of think cmd.",
+	Long: `A longer description of think command.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("think called")
+		fmt.Printf("count args: %d\n", len(args))
+		fmt.Println("args are: " + strings.Join(args, " "))
 	},
 }
 
