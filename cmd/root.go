4dc76ceefc60abb1575246a3ba3fe9c073d15fc6
jarreleye <pawiwi@2980.com>
2020-08-20 17:32:55 +0800

    Born

--- cmd/root.go
+++ cmd/root.go
@@ -42,7 +42,9 @@ This application is a tool to generate the needed files
 to quickly create a cli application.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
-	//	Run: func(cmd *cobra.Command, args []string) { },
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Born to code!")
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
